28616f3017f5047fceeefd0c9c4f8db3d4e27942
Subhankar Paul <144545235+starlox0@users.noreply.github.com>
2026-08-28 19:43:14 +0530

    Update index.js

--- src/theme/BlogListPage/index.js
+++ src/theme/BlogListPage/index.js
@@ -1,1 +1,64 @@
+import React, {useState} from 'react';
+import clsx from 'clsx';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import {
+  PageMetadata,
+  HtmlClassNameProvider,
+  ThemeClassNames,
+} from '@docusaurus/theme-common';
+import BlogLayout from '@theme/BlogLayout';
+import BlogListPaginator from '@theme/BlogListPaginator';
+import SearchMetadata from '@theme/SearchMetadata';
+import BlogPostItems from '@theme/BlogPostItems';
+import BlogListPageStructuredData from '@theme/BlogListPage/StructuredData';
+import BlogTabs from '@site/src/components/BlogTabs';
+import MediumPosts from '@site/src/components/MediumPosts';
+function BlogListPageMetadata(props) {
+  const {metadata} = props;
+  const {
+    siteConfig: {title: siteTitle},
+  } = useDocusaurusContext();
+  const {blogDescription, blogTitle, permalink} = metadata;
+  const isBlogOnlyMode = permalink === '/';
+  const title = isBlogOnlyMode ? siteTitle : blogTitle;
+  return (
+    <>
+      <PageMetadata title={title} description={blogDescription} />
+      <SearchMetadata tag="blog_posts_list" />
+    </>
+  );
+}
+function BlogListPageContent(props) {
+  const {metadata, items, sidebar} = props;
+  const [activeTab, setActiveTab] = useState('mine');
 
+  if (activeTab === 'mine') {
+    return (
+      <BlogLayout>
+        <BlogTabs activeTab={activeTab} onChange={setActiveTab} />
+        <MediumPosts />
+      </BlogLayout>
+    );
+  }
+
+  return (
+    <BlogLayout sidebar={sidebar}>
+      <BlogTabs activeTab={activeTab} onChange={setActiveTab} />
+      <BlogPostItems items={items} />
+      <BlogListPaginator metadata={metadata} />
+    </BlogLayout>
+  );
+}
+export default function BlogListPage(props) {
+  return (
+    <HtmlClassNameProvider
+      className={clsx(
+        ThemeClassNames.wrapper.blogPages,
+        ThemeClassNames.page.blogListPage,
+      )}>
+      <BlogListPageMetadata {...props} />
+      <BlogListPageStructuredData {...props} />
+      <BlogListPageContent {...props} />
+    </HtmlClassNameProvider>
+  );
+}
